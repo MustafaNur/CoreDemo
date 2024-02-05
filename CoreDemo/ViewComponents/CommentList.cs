13a4ed69550111ac5b7d251614546635daffba51
MustafaNur <mustafa@drupart.co.uk>
2024-02-06 01:11:44 +0300

    list of comments
    Using Parameters with View Component & list of comments

---
+++ CoreDemo/ViewComponents/CommentList.cs
@@ -0,0 +1,32 @@
+﻿using CoreDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CoreDemo.ViewComponents
+{
+	public class CommentList:ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			var commentValues = new List<UserComment>
+			{
+				new UserComment
+				{
+					ID= 1,
+					UserName="Mustafa"
+				},
+				new UserComment
+				{
+					ID= 2,
+					UserName="Serkan"
+				},
+				new UserComment
+				{
+					ID= 3,
+					UserName="Selin"
+				}
+			};
+			return View(commentValues);
+		}
+	}
+}
